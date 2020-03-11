1a38292d85bccf2686e149538e66e7e4f0a402b0
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-03-11 15:32:53 +0000

    add blockchain.last_block to function call

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -125,7 +125,7 @@ blockchain = Blockchain()
 @app.route('/mine', methods=['GET'])
 def mine():
     # Run the proof of work algorithm to get the next proof
-    proof = blockchain.proof_of_work()
+    proof = blockchain.proof_of_work(blockchain.last_block)
 
     # Forge the new Block by adding it to the chain with the proof
     previous_hash = blockchain.hash(blockchain.last_block)
